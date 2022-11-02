c2a171028dafbd1ddc713d12d42801497f7a7179
Antonio <ventura_antonio@hotmail.com>
2022-11-02 15:26:53 +0100

    modificado

--- db/posts.js
+++ db/posts.js
@@ -1,5 +1,63 @@
-// const { generateError } = require('../helpers');
+const { generateError } = require('../helpers');
 const { getConnection } = require('./db');
+
+const deteleTweetById = async (id) => {
+  let connection;
+
+  try {
+    connection = await getConnection();
+
+    await connection.query(
+      `
+      DELETE FROM posts WHERE id = ?
+    `,
+      [id]
+    );
+
+    return;
+  } finally {
+    if (connection) connection.release();
+  }
+};
+
+const getPostById = async (id) => {
+  let connection;
+
+  try {
+    connection = await getConnection();
+
+    const [result] = await connection.query(
+      `
+      SELECT * FROM tweets WHERE id = ?
+    `,
+      [id]
+    );
+
+    if (result.length === 0) {
+      throw generateError(`El post con id: ${id} no existe`, 404);
+    }
+
+    return result[0];
+  } finally {
+    if (connection) connection.release();
+  }
+};
+
+const getAllPosts = async () => {
+  let connection;
+
+  try {
+    connection = await getConnection();
+
+    const [result] = await connection.query(`
+      SELECT * FROM posts ORDER BY created_at DESC
+    `);
+
+    return result;
+  } finally {
+    if (connection) connection.release();
+  }
+};
 
 const createPost = async (userId, text, image = '') => {
   let connection;
@@ -9,7 +67,7 @@ const createPost = async (userId, text, image = '') => {
 
     const [result] = await connection.query(
       `
-      INSERT INTO posts(user_id,text,image)
+      INSERT INTO tweets (user_id, text, image)
       VALUES(?,?,?)
     `,
       [userId, text, image]
@@ -21,14 +79,9 @@ const createPost = async (userId, text, image = '') => {
   }
 };
 
-const getAllPosts = async () => {
-  return [];
-};
-
 module.exports = {
   createPost,
   getAllPosts,
-  // getPostById,
-  // detelePostById,
-  // getPostByText,
+  getPostById,
+  deteleTweetById,
 };
